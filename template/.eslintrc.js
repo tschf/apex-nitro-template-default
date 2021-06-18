fb5f6e07be9a5fdd1baf19b149db3073b4c669d1
Trent Schafer <trent.schafer@gmail.com>
2021-06-18 17:11:57 +1000

    Add typescript linting

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -11,6 +11,36 @@ const eslintConfig = {
     es6: true,
     node: true,
   },
+  rules: {
+    "indent": ["error", 2],
+    "prefer-const": "warn"
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: '@typescript-eslint/parser',
+      plugins: [
+        '@typescript-eslint'
+      ],
+      extends: [
+        'eslint:recommended',
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+      ],
+      parserOptions: {
+        ecmaVersion: 10,
+        sourceType: 'module',
+      },
+      env: {
+        browser: true,
+        es6: true,
+        node: true,
+      },
+      rules: {
+      },
+
+    }
+  ]
 };
 
 eslintConfig.globals = apexnitroConfig.external.reduce((a, b) => ((a[b] = 'readonly'), a), {});
