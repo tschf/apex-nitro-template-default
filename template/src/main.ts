ceecb8037e49cb0ca2ffd08210fd85a5364497da
Trent Schafer <trent.schafer@gmail.com>
2021-06-18 17:11:33 +1000

    Remove onload code

--- template/src/main.ts
+++ template/src/main.ts
@@ -3,11 +3,3 @@ const version = "1.0.0";
 export {
   version
 };
-
-apex.jQuery(function(){
-  const pageId = Number(document.getElementById("pFlowStepId").nodeValue);
-	const appId = Number(document.getElementById("pFlowId").nodeValue);
-
-  // TODO: On load page logic
-
-});
